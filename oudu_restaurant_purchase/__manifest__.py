e11729bd1e6ceda1fe78acff744df7f75bc3b1a7
Jason Zou <zou.jason@qq.com>
2025-10-17 16:48:26 +0800

    增加餐厅采购订单模块，在移动端实现完整采购功能

--- oudu_restaurant_purchase/__manifest__.py
+++ oudu_restaurant_purchase/__manifest__.py
@@ -24,7 +24,7 @@
     'email': 'zou.jason@qq.com',
     'website': 'http://www.duodoo.tech',
     'category': 'Purchases',
-    'price': 250,
+    'price': 300,
     'currency': 'USD',
     'depends': [
         'purchase',
