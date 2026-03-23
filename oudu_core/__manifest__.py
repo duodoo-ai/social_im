1af8a49d6ad97dadb78b4a2307be77cbcf51870d
Jason Zou <zou.jason@qq.com>
2026-03-23 09:14:16 +0800

    提交工作文件 20260323

---
+++ oudu_core/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Core Extend(RTX)",
+
+    'summary': """
+        Basic modules of Odoo business
+    """,
+
+    'description': """Basic modules of Odoo business
+                    More Support：
+                    18951631470
+                    zou.jason@qq.com
+                    """,
+
+    'author': "Jason Zou",
+    "website": "www.duodoo.tech",
+
+    'category': 'oudu',
+    'version': '1.0',
+
+    'depends': ['base', 'mail'],
+
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    "license": "AGPL-3",
+}
